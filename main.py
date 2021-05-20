0454e78fc2241109e8891269083bc17b824277a8
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-20 12:44:18 +0100

    Added answers

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+def nplays( s, n, p ) : 
+    nt = 0
+    while( s>0 and s<n ) :
+        nt = nt+1
+        if np.random.uniform(0,1)<p : s = s + 1 
+        else : s = s - 1 
+    return nt
